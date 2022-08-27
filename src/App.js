54da59216c5406a13fd1dc7c15c3006f0e43dfee
AmineKherroubi <makherroubi@gmail.com>
2022-08-27 17:18:12 +0100

    checkpoint react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { FormControl, FormGroup } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='container'>
+      <Form className='form'>
+
+        <h1>Contactez Nous</h1>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Nom</Form.Label>
+          <Form.Control type="text" placeholder="Votre Nom" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Addresse Email</Form.Label>
+          <Form.Control type="email" placeholder="nom@exemple.com" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">Envoyer</Button>
+
+      </Form>
+
+    </Container>
   );
 }
 
